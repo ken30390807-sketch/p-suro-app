78b269519ebc83082e7ab490bdcdc5140bef2ced
jf8jy7dg8w-stack <ken30390807@gmail.com>
2026-09-25 23:29:27 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function Home() {
-return (
-    'メニュー'
+  return (
+    <main>
+      <h1>設定推測カウンター</h1>
 
-);
+      <Link href="/lycoris">
+        リコリス・リコイル →
+      </Link>
+    </main>
+  );
 }
